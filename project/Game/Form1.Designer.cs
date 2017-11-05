068565f82e123fa4371cb6c2e0af9591f1a9cc2d
aleksusklim <aleksusklim@gmail.com>
2017-11-05 14:28:10 +0500

    -

---
+++ project/Game/Form1.Designer.cs
@@ -0,0 +1,300 @@
+﻿namespace Game
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Требуется переменная конструктора.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Освободить все используемые ресурсы.
+        /// </summary>
+        /// <param name="disposing">истинно, если управляемый ресурс должен быть удален; иначе ложно.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Код, автоматически созданный конструктором форм Windows
+
+        /// <summary>
+        /// Обязательный метод для поддержки конструктора - не изменяйте
+        /// содержимое данного метода при помощи редактора кода.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.components = new System.ComponentModel.Container();
+            System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(Form1));
+            this.player = new System.Windows.Forms.PictureBox();
+            this.step = new System.Windows.Forms.Timer(this.components);
+            this.keyer = new System.Windows.Forms.Button();
+            this.left = new System.Windows.Forms.PictureBox();
+            this.right = new System.Windows.Forms.PictureBox();
+            this.down = new System.Windows.Forms.PictureBox();
+            this.enemy1 = new System.Windows.Forms.PictureBox();
+            this.enemy2 = new System.Windows.Forms.PictureBox();
+            this.enemy3 = new System.Windows.Forms.PictureBox();
+            this.enemy4 = new System.Windows.Forms.PictureBox();
+            this.enemy5 = new System.Windows.Forms.PictureBox();
+            this.life = new System.Windows.Forms.ProgressBar();
+            this.boss = new System.Windows.Forms.PictureBox();
+            this.bosser = new System.Windows.Forms.Timer(this.components);
+            this.up = new System.Windows.Forms.PictureBox();
+            this.bullet = new System.Windows.Forms.PictureBox();
+            this.time = new System.Windows.Forms.ProgressBar();
+            this.timer = new System.Windows.Forms.Timer(this.components);
+            ((System.ComponentModel.ISupportInitialize)(this.player)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.left)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.right)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.down)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.enemy1)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.enemy2)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.enemy3)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.enemy4)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.enemy5)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.boss)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.up)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.bullet)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // player
+            // 
+            this.player.Image = ((System.Drawing.Image)(resources.GetObject("player.Image")));
+            this.player.Location = new System.Drawing.Point(21, 199);
+            this.player.Name = "player";
+            this.player.Size = new System.Drawing.Size(32, 32);
+            this.player.SizeMode = System.Windows.Forms.PictureBoxSizeMode.StretchImage;
+            this.player.TabIndex = 0;
+            this.player.TabStop = false;
+            // 
+            // step
+            // 
+            this.step.Interval = 10;
+            this.step.Tick += new System.EventHandler(this.step_Tick);
+            // 
+            // keyer
+            // 
+            this.keyer.Location = new System.Drawing.Point(613, 313);
+            this.keyer.Name = "keyer";
+            this.keyer.Size = new System.Drawing.Size(10, 14);
+            this.keyer.TabIndex = 3;
+            this.keyer.Text = "keyer";
+            this.keyer.UseVisualStyleBackColor = true;
+            this.keyer.KeyDown += new System.Windows.Forms.KeyEventHandler(this.keyer_KeyDown);
+            // 
+            // left
+            // 
+            this.left.Image = ((System.Drawing.Image)(resources.GetObject("left.Image")));
+            this.left.Location = new System.Drawing.Point(0, 51);
+            this.left.Name = "left";
+            this.left.Size = new System.Drawing.Size(32, 32);
+            this.left.SizeMode = System.Windows.Forms.PictureBoxSizeMode.StretchImage;
+            this.left.TabIndex = 6;
+            this.left.TabStop = false;
+            this.left.Visible = false;
+            // 
+            // right
+            // 
+            this.right.Image = ((System.Drawing.Image)(resources.GetObject("right.Image")));
+            this.right.Location = new System.Drawing.Point(602, 255);
+            this.right.Name = "right";
+            this.right.Size = new System.Drawing.Size(32, 32);
+            this.right.SizeMode = System.Windows.Forms.PictureBoxSizeMode.StretchImage;
+            this.right.TabIndex = 7;
+            this.right.TabStop = false;
+            this.right.Visible = false;
+            // 
+            // down
+            // 
+            this.down.Image = ((System.Drawing.Image)(resources.GetObject("down.Image")));
+            this.down.Location = new System.Drawing.Point(0, 275);
+            this.down.Name = "down";
+            this.down.Size = new System.Drawing.Size(32, 32);
+            this.down.SizeMode = System.Windows.Forms.PictureBoxSizeMode.StretchImage;
+            this.down.TabIndex = 8;
+            this.down.TabStop = false;
+            this.down.Visible = false;
+            // 
+            // enemy1
+            // 
+            this.enemy1.Image = ((System.Drawing.Image)(resources.GetObject("enemy1.Image")));
+            this.enemy1.Location = new System.Drawing.Point(501, 272);
+            this.enemy1.Name = "enemy1";
+            this.enemy1.Size = new System.Drawing.Size(32, 32);
+            this.enemy1.SizeMode = System.Windows.Forms.PictureBoxSizeMode.StretchImage;
+            this.enemy1.TabIndex = 9;
+            this.enemy1.TabStop = false;
+            // 
+            // enemy2
+            // 
+            this.enemy2.Image = ((System.Drawing.Image)(resources.GetObject("enemy2.Image")));
+            this.enemy2.Location = new System.Drawing.Point(287, 228);
+            this.enemy2.Name = "enemy2";
+            this.enemy2.Size = new System.Drawing.Size(32, 32);
+            this.enemy2.SizeMode = System.Windows.Forms.PictureBoxSizeMode.StretchImage;
+            this.enemy2.TabIndex = 10;
+            this.enemy2.TabStop = false;
+            // 
+            // enemy3
+            // 
+            this.enemy3.Image = ((System.Drawing.Image)(resources.GetObject("enemy3.Image")));
+            this.enemy3.Location = new System.Drawing.Point(138, 185);
+            this.enemy3.Name = "enemy3";
+            this.enemy3.Size = new System.Drawing.Size(32, 32);
+            this.enemy3.SizeMode = System.Windows.Forms.PictureBoxSizeMode.StretchImage;
+            this.enemy3.TabIndex = 10;
+            this.enemy3.TabStop = false;
+            // 
+            // enemy4
+            // 
+            this.enemy4.Image = ((System.Drawing.Image)(resources.GetObject("enemy4.Image")));
+            this.enemy4.Location = new System.Drawing.Point(370, 141);
+            this.enemy4.Name = "enemy4";
+            this.enemy4.Size = new System.Drawing.Size(32, 32);
+            this.enemy4.SizeMode = System.Windows.Forms.PictureBoxSizeMode.StretchImage;
+            this.enemy4.TabIndex = 10;
+            this.enemy4.TabStop = false;
+            // 
+            // enemy5
+            // 
+            this.enemy5.Image = ((System.Drawing.Image)(resources.GetObject("enemy5.Image")));
+            this.enemy5.Location = new System.Drawing.Point(470, 96);
+            this.enemy5.Name = "enemy5";
+            this.enemy5.Size = new System.Drawing.Size(32, 32);
+            this.enemy5.SizeMode = System.Windows.Forms.PictureBoxSizeMode.StretchImage;
+            this.enemy5.TabIndex = 10;
+            this.enemy5.TabStop = false;
+            // 
+            // life
+            // 
+            this.life.Location = new System.Drawing.Point(7, 6);
+            this.life.Name = "life";
+            this.life.Size = new System.Drawing.Size(623, 17);
+            this.life.Step = 1;
+            this.life.Style = System.Windows.Forms.ProgressBarStyle.Continuous;
+            this.life.TabIndex = 11;
+            this.life.Value = 50;
+            // 
+            // boss
+            // 
+            this.boss.Image = ((System.Drawing.Image)(resources.GetObject("boss.Image")));
+            this.boss.Location = new System.Drawing.Point(181, 51);
+            this.boss.Name = "boss";
+            this.boss.Size = new System.Drawing.Size(32, 32);
+            this.boss.SizeMode = System.Windows.Forms.PictureBoxSizeMode.StretchImage;
+            this.boss.TabIndex = 12;
+            this.boss.TabStop = false;
+            // 
+            // bosser
+            // 
+            this.bosser.Interval = 1000;
+            this.bosser.Tick += new System.EventHandler(this.bosser_Tick);
+            // 
+            // up
+            // 
+            this.up.Image = ((System.Drawing.Image)(resources.GetObject("up.Image")));
+            this.up.Location = new System.Drawing.Point(602, 40);
+            this.up.Name = "up";
+            this.up.Size = new System.Drawing.Size(32, 32);
+            this.up.SizeMode = System.Windows.Forms.PictureBoxSizeMode.StretchImage;
+            this.up.TabIndex = 13;
+            this.up.TabStop = false;
+            this.up.Visible = false;
+            // 
+            // bullet
+            // 
+            this.bullet.Image = ((System.Drawing.Image)(resources.GetObject("bullet.Image")));
+            this.bullet.Location = new System.Drawing.Point(287, 96);
+            this.bullet.Name = "bullet";
+            this.bullet.Size = new System.Drawing.Size(32, 32);
+            this.bullet.SizeMode = System.Windows.Forms.PictureBoxSizeMode.StretchImage;
+            this.bullet.TabIndex = 14;
+            this.bullet.TabStop = false;
+            // 
+            // time
+            // 
+            this.time.Enabled = false;
+            this.time.Location = new System.Drawing.Point(4, 313);
+            this.time.Maximum = 80;
+            this.time.Name = "time";
+            this.time.RightToLeftLayout = true;
+            this.time.Size = new System.Drawing.Size(626, 17);
+            this.time.Step = 1;
+            this.time.TabIndex = 15;
+            this.time.Value = 10;
+            // 
+            // timer
+            // 
+            this.timer.Interval = 500;
+            this.timer.Tick += new System.EventHandler(this.timer_Tick);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(635, 332);
+            this.Controls.Add(this.player);
+            this.Controls.Add(this.up);
+            this.Controls.Add(this.boss);
+            this.Controls.Add(this.enemy5);
+            this.Controls.Add(this.enemy4);
+            this.Controls.Add(this.enemy3);
+            this.Controls.Add(this.enemy2);
+            this.Controls.Add(this.enemy1);
+            this.Controls.Add(this.down);
+            this.Controls.Add(this.right);
+            this.Controls.Add(this.left);
+            this.Controls.Add(this.life);
+            this.Controls.Add(this.bullet);
+            this.Controls.Add(this.time);
+            this.Controls.Add(this.keyer);
+            this.FormBorderStyle = System.Windows.Forms.FormBorderStyle.Fixed3D;
+            this.MaximizeBox = false;
+            this.Name = "Form1";
+            this.Text = "!";
+            this.Load += new System.EventHandler(this.Form1_Load);
+            this.FormClosed += new System.Windows.Forms.FormClosedEventHandler(this.Form1_FormClosed);
+            ((System.ComponentModel.ISupportInitialize)(this.player)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.left)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.right)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.down)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.enemy1)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.enemy2)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.enemy3)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.enemy4)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.enemy5)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.boss)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.up)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.bullet)).EndInit();
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.PictureBox player;
+        private System.Windows.Forms.Timer step;
+        private System.Windows.Forms.Button keyer;
+        private System.Windows.Forms.PictureBox left;
+        private System.Windows.Forms.PictureBox right;
+        private System.Windows.Forms.PictureBox down;
+        private System.Windows.Forms.PictureBox enemy1;
+        private System.Windows.Forms.PictureBox enemy2;
+        private System.Windows.Forms.PictureBox enemy3;
+        private System.Windows.Forms.PictureBox enemy4;
+        private System.Windows.Forms.PictureBox enemy5;
+        private System.Windows.Forms.ProgressBar life;
+        private System.Windows.Forms.PictureBox boss;
+        private System.Windows.Forms.Timer bosser;
+        private System.Windows.Forms.PictureBox up;
+        private System.Windows.Forms.PictureBox bullet;
+        private System.Windows.Forms.ProgressBar time;
+        private System.Windows.Forms.Timer timer;
+    }
+}
+
---
+++ project/Game/Form1.cs
@@ -0,0 +1,261 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Windows.Forms;
+
+namespace Game
+{
+ 
+    public partial class Form1 : Form
+    {
+        double gravity = 0.05;
+        double vspeed = 0;
+        double hspeed = 0;
+        double posx;
+        double posy;
+        double maxhor = 4;
+        double maxver = 5;
+        double xinc = 0.2;
+        double horinc = 0.2;
+        double wid = 32;
+        Random aaa = new Random();
+        double mrd = 3;
+        double enemymax=4.5;
+        double enemymin = 0.7;
+        double health = 50;
+        double pax = 0.5;
+        double bpax = 2;
+        double bossy;
+        double bossysp;
+        double bullysp;
+        double bullgrav = 1;
+        double bully;
+        int mode = 0;
+        int enemax = 6;
+        PictureBox[] enemy = new PictureBox[10];
+        double[] esp = new double[10];
+        double[] elx = new double[10];
+
+        private void make_enemy(int ii)
+        {
+            double o;
+            o = (aaa.NextDouble() * mrd) - mrd/2; 
+            elx[ii]-=esp[ii];
+            esp[ii] += o;
+            esp[ii] = -esp[ii];
+            if (esp[ii] > enemymax) esp[ii] = enemymax;
+            if (esp[ii] < -enemymax) esp[ii] = -enemymax;
+            if ((esp[ii] > -enemymin) && (esp[ii]<0)) esp[ii] = -enemymin;
+            if ((esp[ii] < enemymin) && (esp[ii]>0)) esp[ii] = enemymin;
+          ;
+        }
+        public Form1()
+        {
+            InitializeComponent();
+            enemy[1] = enemy1;
+            enemy[2] = enemy2;
+            enemy[3] = enemy3;
+            enemy[4] = enemy4;
+            enemy[5] = enemy5;
+            enemy[6] = boss;
+            bossy = boss.Location.Y;
+            mode = 0;
+            bullet.Visible = false;
+            for (int i = 1; i <= enemax; i += 1)
+            {
+                esp[i] = 3.5;
+                elx[i] = enemy[i].Location.X;
+               make_enemy(i);
+            }
+            health = 50;
+            time.Value = time.Maximum;
+             MessageBox.Show(
+                 "Игра «Убей Босса v1.2»!!  Создатель – Клименко Алексей, 503-09!!\n\nВы – Зелёный квадрат. Управление влево-вправо – 4 - 6 на цифровой клавиатуре. Для движения удерживать кнопку не обязательно.\n\nПрыжок – 8. Стрелять – 5.\n\nОстерегайтесь врагов – синих квадратов! Они будут отнимать жизнь. Жизнь указана в верхней полосе. Если она кончится – вы проиграли!\n\nВаша цель – сбить Босса – голубого квадратика. Стреляйте по нему красными квадратами, когда пролетаете над ним. Это увеличит вашу жизнь. Заполните полоску жизни до конца и победите!\n\nВремя игры ограничено. Если нижняя полоса истечёт, то это будет ваше поражение!\n"
+                 );
+             step.Enabled = true;
+             bosser.Enabled = true;
+             timer.Enabled = true;
+             this.Text="Управление: 4,6,8,5 -- цифровая клавиатура!";
+        }
+        private void step_Tick(object sender, EventArgs e)
+        {
+            if (player.Location.Y < down.Location.Y)
+            {
+                vspeed += gravity;
+            }
+            else
+            {
+                if (vspeed > 0)
+                {
+                    vspeed = 0;
+                    posy = down.Location.Y;
+                    posx = player.Location.X;
+                }
+            }
+            hspeed = Math.Max(-maxhor, Math.Min(maxhor, hspeed + horinc));
+            if ((posx < left.Location.X) ||
+                (posx > right.Location.X))
+            {
+                hspeed = -hspeed;
+                horinc = -horinc;
+            }
+            posy += vspeed;
+            posx += hspeed;
+            player.Location = new Point((int)posx, (int)posy);
+            keyer.Focus();
+            for (int i = 1; i <= enemax; i += 1)
+            { 
+                if ((enemy[i].Location.X<left.Location.X)||
+                    (enemy[i].Location.X > right.Location.X))
+                {
+                    make_enemy(i);
+                }
+                elx[i] += esp[i];
+                enemy[i].Location = new Point((int)elx[i], enemy[i].Location.Y);
+            }
+            for (int i = 1; i <= enemax; i += 1)
+            {
+                if ((player.Location.X < wid+enemy[i].Location.X) &&
+                    (player.Location.X > -wid+enemy[i].Location.X) &&
+                    (player.Location.Y < wid+enemy[i].Location.Y) &&
+                    (player.Location.Y > -wid+enemy[i].Location.Y))
+                {
+                    health -= pax;
+                    if (health >= 0)
+                    {
+                        life.Value = (int)health;
+                    }
+                    else
+                    {
+                        life.Value = 0;
+                        health = 0;
+                        step.Enabled = false;
+                        bosser.Enabled = false;
+                        timer.Enabled = false;
+                        MessageBox.Show("Вы проиграли! Армия Босса сразила вас.");
+                        step.Enabled = true;
+                        bosser.Enabled = true;
+                        timer.Enabled = true;
+                        health = 50;
+                        time.Value = time.Maximum;
+                    }
+                }
+            }
+            bossy += (bossysp - bossy) / 40;
+            boss.Location = new Point(boss.Location.X, (int) bossy);
+            if (mode == 1)
+            {
+                for (int i = 1; i <= enemax; i += 1)
+                {
+                    if ((bullet.Location.X < wid + enemy[i].Location.X) &&
+                      (bullet.Location.X > -wid + enemy[i].Location.X) &&
+                      (bullet.Location.Y < wid + enemy[i].Location.Y) &&
+                      (bullet.Location.Y > -wid + enemy[i].Location.Y))
+                    {
+                        if (enemy[i] == boss)
+                        {
+                            health += bpax;
+                            if (health <= 100)
+                            {
+                                life.Value = (int)health;
+                            }
+                            else
+                            {
+                                life.Value = 100;
+                                health = 100;
+                                step.Enabled = false;
+                                bosser.Enabled = false;
+                                timer.Enabled = false;
+                                MessageBox.Show("Вы победили! Босс повержен.");
+                                step.Enabled = true;
+                                bosser.Enabled = true;
+                                timer.Enabled = true;
+                                health = 50;
+                                time.Value = time.Maximum;
+                            }
+                        }
+                        else
+                        {
+                      mode = 0;
+                        bullet.Visible = false;
+                        }
+                    }
+                }
+                bullysp += bullgrav;
+                bully += bullysp;
+                bullet.Location = new Point(bullet.Location.X, (int)bully);
+                if (bully > down.Location.Y)
+                {
+                    mode = 0;
+                    bullet.Visible = false;
+                }
+            }
+        }
+ 
+        private void Form1_Load(object sender, EventArgs e)
+        {
+                posx = player.Location.X;
+                posy = player.Location.Y;
+        }
+         
+        private void keyer_KeyDown(object sender, KeyEventArgs e)
+        {
+           switch (e.KeyData)
+            {
+       case Keys.NumPad8:
+                if (player.Location.Y >= down.Location.Y - 2)
+                {vspeed = -maxver;} 
+                ; break;
+       case Keys.NumPad4:
+           horinc = -xinc;
+           ; break;
+       case Keys.NumPad6:
+           horinc = +xinc;
+           ; break;
+       case Keys.NumPad5:
+           if (mode == 0)
+           {
+               mode = 1;
+               bullet.Location = player.Location;
+               bully = bullet.Location.Y;
+               bullysp = 0;
+               bullet.Visible = true;
+           }
+           ; break;
+            }
+        }
+        private void bosser_Tick(object sender, EventArgs e)
+        {
+            bossysp = up.Location.Y + aaa.NextDouble() * (down.Location.Y - up.Location.Y);
+        }
+
+        private void timer_Tick(object sender, EventArgs e)
+        {
+            time.Value -= 1;
+            if (time.Value <= 0)
+            {
+                step.Enabled = false;
+                bosser.Enabled = false;
+                timer.Enabled = false;
+                MessageBox.Show("Вы проиграли! Закончилось время");
+                step.Enabled = true;
+                bosser.Enabled = true;
+                timer.Enabled = true;
+                health = 50;
+                time.Value = time.Maximum;
+            }
+        }
+
+        private void Form1_FormClosed(object sender, FormClosedEventArgs e)
+        {
+            step.Enabled = false;
+            bosser.Enabled = false;
+            timer.Enabled = false;
+            MessageBox.Show("Вам понравилось?");
+        }
+    }
+}
